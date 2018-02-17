0fd9897c2b60d363814135932e07cda72a648e8b
Atchyut Nagabhairava <atchyutnagabhairava@gmail.com>
2018-02-17 13:07:07 +0530

    added user registration form

--- app.py
+++ app.py
@@ -6,10 +6,10 @@ Created on Fri Feb 16 13:04:42 2018
 """
 
 from flask import Flask, render_template, flash, redirect, url_for, session, logging, request
-from  data import Articles
-from flask-mysqldb import MySQL
-from wtforms import Form, StringField, TextAreaField, PasswordField, Validators
-from passlib import sha256_crypt
+from data import Articles
+from flask_mysqldb import MySQL
+from wtforms import Form, StringField, TextAreaField, PasswordField, validators
+from passlib.hash import sha256_crypt
 
 
 app = Flask(__name__)
@@ -31,6 +31,24 @@ def articles():
 def article(id):
     return render_template('article.html', id=id)
 
+class RegistrationForm(Form):
+    name = StringField('Name', [validators.Length(min=1, max=50)])
+    username = StringField('Username', [validators.Length(min=4, max=25)])
+    email = StringField('email', [validators.Length(min=5, max=50)])
+    password = PasswordField('password', [
+        validators.DataRequired(),
+        validators.EqualTo('confirm', message='passwords do not match')
+        ])
+    confirm = PasswordField('confirm password', [
+        validators.input_required
+    ])
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm(request.form)
+    if request.method == 'POST' and form.validate():
+        return render_template('register.html')
+    return render_template('register.html', form=form)
 
 if __name__ == "__main__":
     app.run(debug=True)
