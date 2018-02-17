3e90002f7bc9635552fd2e2c3c1feb5ea609f859
Atchyut Nagabhairava <atchyutnagabhairava@gmail.com>
2018-02-17 19:08:09 +0530

    added entire login functionality for user

--- app.py
+++ app.py
@@ -24,24 +24,60 @@ app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 #init MYSQL
 mysql = MySQL(app)
 
-
 Articles = Articles()
 
+#Route for home
 @app.route('/')
 def home():
     return render_template('home.html')
 
+#Route for about
 @app.route('/about')
 def about():
     return render_template('about.html')
 
+#Route for articles
 @app.route('/articles')
 def articles():
     return render_template('articles.html', articles = Articles)
 
+#Route for article page
 @app.route('/articles/<string:id>/')
 def article(id):
     return render_template('article.html', id=id)
+
+#User Login
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password_entered = request.form['password']
+
+        #create cursor
+        cur = mysql.connection.cursor()
+
+        #get the username
+        username = cur.execute("SELECT * FROM users WHERE username = %s",[username]) 
+        
+        if username > 0:
+            #get the first user detials
+            data = cur.fetchone()
+            #find password from the user details
+            password = data['password']
+            
+            #compare passwords
+            if sha256_crypt.verify(password_entered, password):
+                #password verified
+                session['logged_in'] = True
+                session['username'] = username
+                flash('You have successfully loggedin!', 'success')
+                return redirect(url_for('dashboard'))
+            else:
+                flash('You have entered a wrong password', 'danger')
+        #condition if no user found
+        else:
+            flash('No user found with that username', 'danger')
+    return render_template('login.html')
 
 #User form registration form class
 class RegistrationForm(Form):
@@ -75,10 +111,22 @@ def register():
         #Close connection
         cur.close()
 
-        flash('You are now registerred and can log in', 'success')
-        return redirect(url_for('articles'))
+        flash('You are now registered and can log in', 'success')
+        return redirect(url_for('login'))
 
     return render_template('register.html', form=form)
+
+#route for logout
+@app.route('/logout')
+def logout():
+    session.clear()
+    flash('You have successfully logged out', 'success')
+    return redirect(url_for('login'))
+
+#routes for dashboard
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
 
 if __name__ == "__main__":
     app.secret_key = 'secret123'
